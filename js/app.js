fd36ca4dde2bc4d443fff843c1d51ffa6bb19389
STEVEN BACHIMONT <stevenbachimont@gmail.com>
2024-03-10 17:51:16 +0100

    petites modifs et rédaction des commentaires

--- js/app.js
+++ js/app.js
@@ -1,16 +1,19 @@
 
+// ici c'est les interactions de type quizz.
+// Les réponses choisies par l'utilisateur pourront être trétées et les actions en fonctions seront faites en temps réel.
 
-function checkAnswer1(answer) {
-  let resultElement = document.getElementById('result1');
-  if (answer === 'Proche') {
-    resultElement.textContent = 'Alors faites vos lacets ! on y va a pied !';
-    alert("Alors faites vos lacets ! on y va a pied !");
-  } else if (answer === 'PasProche'){
-    resultElement.textContent = 'Preparez la bicyclette !';
-    alert("Preparez la bicyclette !")
-  }
+// Ci-dessous, la répétitionde la même fonction adapté à chaque question.
+function checkAnswer1(answer) {// fonction pour la question
+  let resultElement = document.getElementById('result1');// si réponse 1,
+  if (answer === 'Proche') { // la question a l'id 'proche'
+    resultElement.textContent = 'Alors faites vos lacets ! on y va a pied !';// appel de commentaire en retour
+    alert("Alors faites vos lacets ! on y va a pied !");// Alerte popup pour afficher le retour de commentaire.
+  } else if (answer === 'PasProche'){// réponse 2,
+    resultElement.textContent = 'Preparez la bicyclette !';// appel de commentaire en retour
+    alert("Preparez la bicyclette !")// Alerte popup pour afficher le retour de commentaire.
+  }// à noter que le message en alerte popup ne sert qu'a tester le fonctionnement, il faudra le remplacer par la logique de tri des restaurents
 }
-
+// ci-dessous, idem pour les autres questions...
 function checkAnswer2(answer) {
   let resultElement = document.getElementById('result2');
   if (answer === 'Gros') {
