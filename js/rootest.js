ee96e90ea437fd651a088f317a4dd37218891197
STEVEN BACHIMONT <142581911+stevenbachimont@users.noreply.github.com>
2024-03-21 20:10:38 +0100

    de mon coté c'est bon

--- js/rootest.js
+++ js/rootest.js
@@ -1,10 +1,12 @@
+// pour m'amuser, et amuser Lucas qui aime cliquer pour generer des conneries, j'ai fait une petite intervention
+//de randomisation des couleurs du root css en cliquant sur le logo:
 
 function getRandomColor() {
 
     const lettres = '0123456789ABCDEF';
     let color = '#';
-    for (let i = 0; i < 6; i++) {
-        color += lettres[Math.floor(Math.random() * 16)];
+    for (let i = 0; i < 6; i++) {// on peut me féliciter de l'utilisation d'une boucle for !!
+        color += lettres[Math.floor(Math.random() * 16)];// j'aime bien le random car il donne l'impression à l'utilisteur que la machine fait des choix
     }
     return color;
 }
@@ -13,27 +15,18 @@ function updateRootColors() {
     document.documentElement.style.setProperty('--couleur1', getRandomColor());
     document.documentElement.style.setProperty('--couleur2', getRandomColor());
     document.documentElement.style.setProperty('--couleur3', getRandomColor());
-    document.documentElement.style.setProperty('--couleur4', '#000000');
-
+    document.documentElement.style.setProperty('--couleur4', '#000000');// la couleur 4 est fixée au noir ca c'est celle du texte
     document.documentElement.style.setProperty('--couleur5', getRandomColor());
 }
 
+//donc on clique sur quoi ? sur le logo..
 window.addEventListener('DOMContentLoaded', () => {
     const image = document.getElementById('logo');
     image.addEventListener('click', updateRootColors);
 });
 
 
-const reloadLinks = document.querySelectorAll('.reload-link');
 
-reloadLinks.forEach(link => {
-    link.addEventListener('click', function(event) {
-        // Recharge la page après un court délai
-        setTimeout(() => {
-            location.reload();
-        }, 100);
-    });
-});
 
 
 
